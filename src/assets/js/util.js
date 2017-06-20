dd3b6d0ede4316ad71cbf86cebd4eeeef7fb2716
fatzhou <fatzhou@fatzhoudeMacBook-Pro.local>
2017-06-20 22:55:47 +0800

    first commit

---
+++ src/assets/js/util.js
@@ -0,0 +1,68 @@
+var api = {
+    host: 'https://www.wxpuu.com',
+    mobile: '/api/shop/register',
+    fileApi: '/api/upload',
+    categoryList: '/api/shop/classquery',
+    buildShop: '/api/shop/update',
+    buildProduct: '/api/shop/dealprod',
+    addCategory: '/api/shop/dealclass',
+    itemsList: '/api/shop/prodlist',
+    shopInfo: '/api/shop/query',
+    shopUpdate: '/api/shop/update',
+    queryShop: '/api/shop/query',
+    queryOrder: '/api/shop/orderquery',
+    userInfo: '/api/mp/getuserinfo',
+    dealOrder: '/api/shop/orderdeal',
+    userInfo: '/api/mp/getuserinfo',
+    getToken: '/api/mp/getAuthInfoByCode',
+    getXcxToken: '/api/mp/getXCXAccessToken',
+    getQrcode: '/api/mp/getXCXQRCode'
+};
+
+var validator = {
+    email: function(str) {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(str);
+    },
+    telephone: function(str) {
+        var re = /^0?1[3|4|5|8|7][0-9]\d{8}$/;
+        return re.test(str);
+    }
+};
+
+function checkForm(data) {
+    var flag = data.every(function(item) {
+        if (!item.data) {
+            alert(item.name + '不能为空');
+            return false;
+        }
+        if (validator[item.type]) {
+            var tmpFlag = validator[item.type](item.data);
+            if (!tmpFlag) {
+                alert(item.name + '填写不符合规则');
+            }
+            return tmpFlag;
+        }
+        return true;
+    });
+    return flag;
+}
+
+function getUrlKey(name) {
+    var url = location.search; //获取url中"?"符后的字串
+    var theRequest = new Object();
+    if (url.indexOf("?") != -1) {
+        var str = url.substr(1),
+            strs = str.split("&");
+        for (var i = 0; i < strs.length; i++) {
+            theRequest[strs[i].split("=")[0]] = unescape(strs[i].split("=")[1].replace(/\//g, ''));
+        }
+    }
+    return theRequest;
+}
+
+export default {
+    api: api,
+    checkForm: checkForm,
+    getUrlKey: getUrlKey
+}
---
+++ src/components/OrderList.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="container dingdan-wrap yuyue-wrap">
+      <div class="wrap">
+          <div class="weui-form-preview" v-for="item in orderList">
+              <div class="weui-form-preview__hd">
+                  <div class="weui-form-preview__item">
+                      <label class="weui-form-preview__label">预约号</label>
+                      <span class="weui-form-preview__value">{{item.orderno}}</span>
+                  </div>
+              </div>
+              <div class="weui-form-preview__bd">
+                  <div class="weui-form-preview__item">
+                      <label class="weui-form-preview__label">下单时间</label>
+                      <span class="weui-form-preview__value">{{time}}</span>
+                  </div>
+                  <div class="weui-form-preview__item">
+                      <label class="weui-form-preview__label">预约理发师</label>
+                      <span class="weui-form-preview__value">{{item.detail[0].name}}</span>
+                  </div>
+                  <div class="weui-form-preview__item">
+                      <label class="weui-form-preview__label">联系用户</label>
+                      <span class="weui-form-preview__value"><i class="iconfont-dasan-16"></i><span>拨打</span></span>
+                  </div>
+                  <div class="weui-form-preview__item">
+                      <label class="weui-form-preview__label">用户已等待</label>
+                      <span class="weui-form-preview__value"> <span class="statu sta1">30分钟</span></span>
+                  </div>
+              </div>
+              <div class="weui-form-preview__ft">
+                  <a class="weui-btn weui-btn_primary " v-if="item.status == 1 || item.status == 2" href="javascript:" id="">通知Ta到店</a>
+                  <a class="weui-btn weui-btn_primary weui-btn_disabled" v-else href="javascript:" id="">已通知</a>
+              </div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+  import util from '../assets/js/util.js'
+
+export default {
+  name: 'hello',
+  data () {
+    return {
+      url: util.api.host + util.api.queryOrder,
+      dealUrl: util.api.host + util.api.dealOrder,
+      // date: '20170620',
+      pageno: 1,
+      pagesize: 10,
+      orderList: []
+    }
+  },
+
+  activated() {
+    this.queryOrderInfo();
+  },
+
+ methods: {
+    dealOrder(orderno, index) {
+      var postData = {
+        openid: window.info.openid,
+        token: window.info.token,
+        shopid: window.info.shopid,
+        orderno: orderno
+      };
+
+      this.$http.post(this.dealUrl, postData)
+      .then((res)=>{
+        var data = res.body;
+        if(data.code == 0) {
+          this.orderList[index].status = 3;
+        } else {
+          alert(data.msg);
+        }
+      });
+    },
+    computeTime(st) {
+      var dt = new Date(st);
+      var year = dt.getFullYear(),
+          month = dt.getMonth() + 1,
+          day = dt.getDay(),
+          hour = ('00' + dt.getHours()).slice(-2),
+          minutes = ('00' + dt.getMinutes()).slice(-2),
+          seconds = ('00' + dt.getSeconds()).slice(-2);
+      return year + '年' + month + '月' + day + '日' + ' ' + [hour, minutes, seconds].join(':');
+    },
+    queryOrderInfo() {
+      var postData = {
+        openid: window.info.openid,
+        token: window.info.token,
+        shopid: window.info.shopid,
+        pageno: this.pageno,
+        pagesize: this.pagesize
+      };
+
+      this.$http.post(this.url, postData)
+      .then((res)=>{
+        var data = res.body;
+        console.log(data,'orderlist')
+        if(data.code == 0) {
+          var orderList = data.orderlist || [];
+          orderList.forEach((item)=>{
+            item.time = this.computeTime(item.createtime * 1000);
+            if(typeof item.detail === 'string') {
+              item.detail = JSON.parse(item.detail);
+            }
+          });
+
+          this.orderList = orderList;
+        } else {
+          // alert(data.msg);
+        }
+      });
+    },
+      }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less" scoped>
+  @import '../assets/less/tob-index.less';
+  @import '../assets/less/yuyue.less';
+  .container {
+    background: #f3f3f3;
+  }
+</style>
